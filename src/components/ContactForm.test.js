92eff7c8945692eb280ffbb03dc590c17fb5cf9b
Peter Conley <peterdavidconley@gmail.com>
2021-12-02 15:45:47 -0500

    completed step 6, wrote code for 3-5 but they are not passing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -23,38 +23,88 @@ test('renders the contact form header', ()=> {
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
 
     // Arrange
+    render(<ContactForm />)
     // Act
+    const nameErrorMessage = screen.queryByText('Error: firstName must have at least 5 characters.')
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
     // Assert
+    expect(nameErrorMessage).toBeInTheDocument()
 
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
     
     // Arrange
+    render(<ContactForm />)
     // Act
+
+    const fNameErrorMessage = screen.queryByText('Error: firstName must have at least 5 characters.')
+    const lNameErrorMessage = screen.queryByText('Error: lastName is a required field.')
+    const emailErrorMessage = screen.queryByText('Error: email must be a valid email address.')
+
+    // 1. Find button
+    const button = screen.getByRole("button");
+    // 2. Click button
+    userEvent.click(button);
+
     // Assert
+    expect(fNameErrorMessage).toBeInTheDocument()
+    expect(lNameErrorMessage).toBeInTheDocument()
+    expect(emailErrorMessage).toBeInTheDocument()
 
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
     
     // Arrange
+    render(<ContactForm />)
     // Act
+
+    const emailErrorMessage = screen.queryByText('Error: email must be a valid email address.')
+
+    const fNameInput = screen.queryByLabelText('First Name*') 
+    const lNameInput = screen.queryByLabelText('Last Name*') 
+    userEvent.type(fNameInput, "Peter")
+    userEvent.type(lNameInput, "Conley")
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
     // Assert
+
+    expect(emailErrorMessage).toBeInTheDocument()
+
 
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
     
     // Arrange
+    render(<ContactForm />)
     // Act
+
+    const fNameInput = screen.queryByLabelText('First Name*') 
+    const lNameInput = screen.queryByLabelText('Last Name*') 
+    userEvent.type(fNameInput, "Peter")
+    userEvent.type(lNameInput, "Conley")
+    const emailInput = screen.queryByLabelText('Email*') 
+    userEvent.type(emailInput, "Peter")
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    const emailErrorMessage = screen.queryByText('Error: email must be a valid email address.')
+
     // Assert
+
+    expect(emailErrorMessage).toBeInTheDocument();
 
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
     
     // Arrange
+    render(<ContactForm />)
     // Act
     // Assert
 
@@ -63,6 +113,7 @@ test('renders "lastName is a required field" if an last name is not entered and 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
     
     // Arrange
+    render(<ContactForm />)
     // Act
     // Assert
 
@@ -71,6 +122,7 @@ test('renders all firstName, lastName and email text when submitted. Does NOT re
 test('renders all fields text when all fields are submitted.', async () => {
 
     // Arrange
+    render(<ContactForm />)
     // Act
     // Assert
 
